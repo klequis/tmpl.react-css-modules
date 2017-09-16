3cfdb04b912f5e84038043ba9949993229fd7fa8
klequis <klequis@gmail.com>
2017-09-16 14:45:40 -0700

    initial commit

---
+++ src/components/Page/index.jsx
@@ -0,0 +1,13 @@
+// Page
+import React from 'react';
+
+const Page = () => {
+
+  return (
+    <div>
+      <h1>Hellow from Page root component</h1>
+    </div>
+  )
+}
+
+export Page
---
+++ src/store/reducers.js
@@ -0,0 +1,32 @@
+import { combineReducers } from 'redux';
+// import { merge, prepend, dissoc, without } from 'ramda';
+// import { merge } from 'ramda';
+
+export const reducerOne = (state = { id: 0 }, { type, payload }) => {
+  switch (type) {
+    case 'app/currentComponentId':
+      return payload;
+    default:
+      return state;
+  }
+};
+
+export const requests = (state = {}, { type, payload, meta }) => {
+  switch (type) {
+    case 'app/markRequestPending':
+      return merge(state, { [meta.key]: { status: 'pending', error: null } });
+    case 'app/markRequestSuccess':
+      return merge(state, { [meta.key]: { status: 'success', error: null } });
+    case 'app/markRequestFailed':
+      return merge(state, { [meta.key]: { status: 'failure', error: payload } });
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({
+  ui: combineReducers({
+    reducerOne,
+  }),
+  requests,
+})
